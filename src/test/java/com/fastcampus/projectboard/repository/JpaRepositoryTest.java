af743b378863f06f7501f772a31cc9938a32450a
jongwon_Kim <jw909069@naver.com>
2023-01-04 22:55:13 +0900

    #10 - 테스트 코드에 newline 추가
    이래야 깃헙에서 빨간 EOF 이모티콘을 안보게 된다

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -96,8 +96,4 @@ class JpaRepositoryTest {
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
         assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
-
-
-
-
 }
