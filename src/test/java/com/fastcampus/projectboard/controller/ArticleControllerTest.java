3b263f90a81e3abeccffb0ee3f1f95ed9fc7f381
jongwon_Kim <jw909069@naver.com>
2023-01-17 21:51:10 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러에 반영
    스프링 시큐리티가 자동 생성해준 로그인 컨트롤러 테스트
    그리고 시큐리티 설정을 부를수 있도록 @Import 추가

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class) // @WebMvcTest 만 적으면 모든 컨트롤러를 읽어드림, class 를 잡아줌.
 class ArticleControllerTest {
 
